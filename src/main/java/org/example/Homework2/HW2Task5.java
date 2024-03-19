fc9b60e5ccc5242a90f2c67e9dcd53423bcb4b3e
BratProgrammer <travckin.4@yandex.ru>
2024-03-20 01:04:02 +0300

    HomeWork 1:  Task 1 - Done  Task 2 - Done  Task 3 - Done  Task 4 - Done  Task 5 - Done  Task 6 - Done  Task 7 - lose  Task 8 - lose HomeWork 2:  Task 1 - Done  Task 2 - Done  Task 3 - Done  Task 4 - Done  Task 5 - In progress

--- src/main/java/org/example/Homework2/HW2Task5.java
+++ src/main/java/org/example/Homework2/HW2Task5.java
@@ -1,24 +1,185 @@
 package org.example.Homework2;
 
+import org.example.Main;
 import org.example.Task;
+import org.w3c.dom.ls.LSOutput;
 
-import java.util.HashMap;
-import java.util.Scanner;
+import java.net.Inet4Address;
+import java.util.*;
 
 public class HW2Task5 implements Task {
+
     @Override
     public void start() {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
 
-        HashMap<Integer, Integer> berriesProfit = new HashMap<>();
-        HashMap<Integer, int[]> berries = new HashMap<>();
+        ArrayList<Integer> indexesOfPositiveValueBerries = new ArrayList<Integer>();
+        ArrayList<Integer> indexesOfZeroValueBerries = new ArrayList<Integer>();
+        ArrayList<Integer> indexesOfNegativeValueBerries = new ArrayList<Integer>();
+        ArrayList<Integer> profitsOfBerries = new ArrayList<Integer>();
+        ArrayList<Integer> upValues = new ArrayList<Integer>();
+
+        long height = 0;
+        int indexOfEndProfitBerrie = 0;
+
+        StringBuilder indexes = new StringBuilder();
+
 
         for (int i = 1; i <= n; i++) {
             String[] inputValues = scanner.nextLine().split(" ");
 
-            int profit = Integer.parseInt(inputValues[0]) - Integer.parseInt(inputValues[0]);
+            int upValue = Integer.parseInt(inputValues[0]); // Подъём
+            int downValue = Integer.parseInt(inputValues[1]); // Спуск
+
+            if (upValue > downValue) {
+                indexesOfPositiveValueBerries.add(i);
+                height += upValue - downValue;
+                indexes.append(" ").append(i);
+            } else if (upValue == downValue) {
+                indexesOfZeroValueBerries.add(i);
+            } else {
+                indexesOfNegativeValueBerries.add(i);
+            }
+
+            profitsOfBerries.add(upValue - downValue);
+            upValues.add(upValue);
 
         }
+
+        long maxHeight = height;
+
+        for (int i = 1; i <= n; i++) {
+            if (indexesOfPositiveValueBerries.contains(i)) {
+                if (maxHeight <= height + upValues.get(i) - profitsOfBerries.get(i)) {
+                    maxHeight = height + upValues.get(i) - profitsOfBerries.get(i);
+                    indexOfEndProfitBerrie = i;
+                }
+            } else {
+                if (maxHeight < height + upValues.get(i)) {
+                    maxHeight = height + upValues.get(i);
+                    indexOfEndProfitBerrie = i;
+                }
+            }
+        }
+
+        String indexesStr = indexes.toString().replace(" " + indexOfEndProfitBerrie, " ") + " " + indexOfEndProfitBerrie;
+
+        if (indexesOfNegativeValueBerries.contains(indexOfEndProfitBerrie)) {
+            indexesOfNegativeValueBerries.remove((Integer) indexOfEndProfitBerrie);
+        } else if (indexesOfZeroValueBerries.contains(indexOfEndProfitBerrie)) {
+            indexesOfZeroValueBerries.remove((Integer) indexOfEndProfitBerrie);
+        }
+
+        for (int i : indexesOfZeroValueBerries) {
+            indexesStr += " " + i;
+        }
+
+        for (int i : indexesOfNegativeValueBerries) {
+            indexesStr += " " + i;
+        }
+
+        System.out.println(maxHeight);
+        System.out.println(indexesStr);
+
     }
 }
+
+/*
+private ArrayList<int[]> sortByProfit(ArrayList<int[]> list) {
+        for(int i = 0; i < list.size() - 1; i++) {
+            for (int j = 0; j < list.size() - i - 1; j++) {
+                if (getProfit(list.get(j)) < getProfit(list.get(j + 1))) {
+                    int[] buffer = list.get(j);
+
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, buffer);
+                }
+            }
+        }
+        return list;
+    }
+
+    private int getProfit(int[] berrie) {
+        return berrie[1] - berrie[2];
+    }
+
+
+
+*Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        ArrayList<int[]> berries = new ArrayList<int[]>();
+
+        for (int i = 1; i <= n; i++) {
+            String[] inputValues = scanner.nextLine().split(" ");
+            int[] berrie = new int[3];
+            berrie[0] = i; // Номер ягоды
+            berrie[1] = Integer.parseInt(inputValues[0]); // Подъём
+            berrie[2] = Integer.parseInt(inputValues[1]); // Спуск
+            berries.add(berrie);
+        }
+
+        berries = sortByProfit(berries);
+
+        int height = 0;
+        int maxHeight = 0;
+        int maxUpValueFromNoProfitBerries = 0;
+        int indexOfMaxUpValueFromNoProfitBerries = 0;
+        int maxUpValue = 0;
+        int indexOfBarrieWithMaxUpValue = -1;
+        String indexes = "";
+
+        for (int i = 0; i < berries.size(); i++) {
+            if (getProfit(berries.get(i)) > 0) {
+                height += getProfit(berries.get(i));
+                maxHeight = Math.max(maxHeight, height);
+                indexes += " " + berries.get(i)[0];
+            } else {
+                if (maxUpValueFromNoProfitBerries <= berries.get(i)[1]) {
+                    maxUpValueFromNoProfitBerries = berries.get(i)[1];
+                    indexOfMaxUpValueFromNoProfitBerries = i;
+                }
+            }
+
+            if (maxUpValue == berries.get(i)[1]) {
+                if (indexOfBarrieWithMaxUpValue != -1) {
+                    if (getProfit(berries.get(indexOfBarrieWithMaxUpValue)) > getProfit(berries.get(i))) {
+                        indexOfBarrieWithMaxUpValue = i;
+                    }
+                } else {
+                    indexOfBarrieWithMaxUpValue = i;
+                }
+
+            } else if (maxUpValue < berries.get(i)[1]) {
+                indexOfBarrieWithMaxUpValue = i;
+                maxUpValue = berries.get(i)[1];
+            }
+            maxUpValue = Math.max(maxUpValue, berries.get(i)[1]);
+        }
+
+        int result = maxHeight;
+
+        if (maxUpValueFromNoProfitBerries == maxUpValue) {
+            result += maxUpValueFromNoProfitBerries;
+            indexes += " " + berries.get(indexOfMaxUpValueFromNoProfitBerries)[0];
+        } else {
+
+            if (maxUpValueFromNoProfitBerries > maxUpValue - getProfit(berries.get(indexOfBarrieWithMaxUpValue))) {
+                result += maxUpValueFromNoProfitBerries;
+                indexes += " " + berries.get(indexOfMaxUpValueFromNoProfitBerries)[0];
+            } else {
+                result += maxUpValue - getProfit(berries.get(indexOfBarrieWithMaxUpValue));
+                indexes = indexes.replace(" " + berries.get(indexOfBarrieWithMaxUpValue)[0], "");
+                indexes += " " + berries.get(indexOfBarrieWithMaxUpValue)[0];
+            }
+        }
+
+        for (int[] i : berries) {
+            if (indexes.contains(" " + i[0])) { continue; }
+            indexes += " " + i[0];
+        }
+
+        System.out.println(result);
+        System.out.println(indexes.trim());
+* */
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,62 +1,85 @@
 package org.example;
 
 import org.example.Homework2.HW2Task4;
+import org.example.Homework2.HW2Task5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-    private static boolean coordinatesEqual(int[] coordinates1, int[] coordinates2) {
-        return coordinates1[0] == coordinates2[0] && coordinates1[1] == coordinates2[1];
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
-        ArrayList<int[]> coordinatesList = new ArrayList<>();
+
+        ArrayList<Integer> indexesOfPositiveValueBerries = new ArrayList<Integer>();
+        ArrayList<Integer> indexesOfZeroValueBerries = new ArrayList<Integer>();
+        ArrayList<Integer> indexesOfNegativeValueBerries = new ArrayList<Integer>();
+        ArrayList<Integer> profitsOfBerries = new ArrayList<Integer>();
+        ArrayList<Integer> upValues = new ArrayList<Integer>();
+
+        long height = 0;
+        int indexOfEndProfitBerrie = 0;
+
+        StringBuilder indexes = new StringBuilder();
+
 
         for (int i = 0; i < n; i++) {
             String[] inputValues = scanner.nextLine().split(" ");
-            int[] coordinates = new int[2];
-            coordinates[0] = Integer.parseInt(inputValues[0]);
-            coordinates[1] = Integer.parseInt(inputValues[1]);
-            coordinatesList.add(coordinates);
+
+            int upValue = Integer.parseInt(inputValues[0]);
+            int downValue = Integer.parseInt(inputValues[1]);
+
+            if (upValue > downValue) {
+                indexesOfPositiveValueBerries.add(i + 1);
+                height += upValue - downValue;
+                indexes.append(" ").append(i + 1);
+            } else if (upValue == downValue) {
+                indexesOfZeroValueBerries.add(i + 1);
+            } else {
+                indexesOfNegativeValueBerries.add(i + 1);
+            }
+
+            profitsOfBerries.add(upValue - downValue);
+            upValues.add(upValue);
+
         }
 
-        int perimeter = 0;
+        long maxHeight = height;
 
-        for (int[] i : coordinatesList) {
-
-            int perimeterIncrease = 4;
-
-            int[] upper = new int[2];
-            int[] lower = new int[2];
-            int[] left = new int[2];
-            int[] right = new int[2];
-            upper[0] = i[0];
-            upper[1] = i[1] + 1;
-            lower[0] = i[0];
-            lower[1] = i[1] - 1;
-            left[0] = i[0] - 1;
-            left[1] = i[1];
-            right[0] = i[0] + 1;
-            right[1] = i[1];
-
-            for(int[] j : coordinatesList) {
-                if (coordinatesEqual(upper, j)) {
-                    perimeterIncrease -= 1;
-                } else if (coordinatesEqual(right, j)) {
-                    perimeterIncrease -= 1;
-                } else if (coordinatesEqual(lower, j)) {
-                    perimeterIncrease -= 1;
-                } else if (coordinatesEqual(left, j)) {
-                    perimeterIncrease -= 1;
+        for (int i = 0; i < n; i++) {
+            if (indexesOfPositiveValueBerries.contains(i + 1)) {
+                if (maxHeight <= height + upValues.get(i) - profitsOfBerries.get(i)) {
+                    maxHeight = height + upValues.get(i) - profitsOfBerries.get(i);
+                    indexOfEndProfitBerrie = i + 1;
+                }
+            } else {
+                if (maxHeight < height + upValues.get(i)) {
+                    maxHeight = height + upValues.get(i);
+                    indexOfEndProfitBerrie = i + 1;
                 }
             }
-            perimeter += perimeterIncrease;
         }
 
-        System.out.println(perimeter);
+        String indexesStr = indexes.toString().replace(" " + indexOfEndProfitBerrie, " ") + " " + indexOfEndProfitBerrie;
+
+        if (indexesOfNegativeValueBerries.contains(indexOfEndProfitBerrie)) {
+            indexesOfNegativeValueBerries.remove((Integer) indexOfEndProfitBerrie);
+        } else if (indexesOfZeroValueBerries.contains(indexOfEndProfitBerrie)) {
+            indexesOfZeroValueBerries.remove((Integer) indexOfEndProfitBerrie);
+        }
+
+        for (int i : indexesOfZeroValueBerries) {
+            indexesStr += " " + i;
+        }
+
+        for (int i : indexesOfNegativeValueBerries) {
+            indexesStr += " " + i;
+        }
+
+        System.out.println(maxHeight);
+        System.out.println(indexesStr.trim());
     }
 }
