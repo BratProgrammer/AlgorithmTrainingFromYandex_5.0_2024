fc4400f1f4b99d868f807538b3c19e445abd859c
BratProgrammer <travckin.4@yandex.ru>
2024-03-27 12:15:41 +0300

    HomeWork 1:  Task 1 - Done  Task 2 - Done  Task 3 - Done  Task 4 - Done  Task 5 - Done  Task 6 - Done  Task 7 - lose  Task 8 - lose HomeWork 2:  Task 1 - Done  Task 2 - Done  Task 3 - Done  Task 4 - Done  Task 5 - lose  Task 7 - TODO for myself HomeWork 3:  Task 1 - Done  Task 2 - Done  Task 3 - Done  Task 4 - Done  Task 5 - Done

--- src/main/java/org/example/Homework3/HW3Task4.java
+++ src/main/java/org/example/Homework3/HW3Task4.java
@@ -3,6 +3,7 @@ package org.example.Homework3;
 import org.example.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class HW3Task4 implements Task {
@@ -10,11 +11,31 @@ public class HW3Task4 implements Task {
     public void start() {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int k = scanner.nextInt();
+        String[] firstLine = scanner.nextLine().split(" ");
+        int n = Integer.parseInt(firstLine[0]);
+        int k = Integer.parseInt(firstLine[1]);
 
-        int numberForCheck = 0;
-        ArrayList<Integer> numbersForSearchArray = new ArrayList<>();
+        String[] numbers = scanner.nextLine().split(" ");
+        boolean result = false;
 
+        HashMap<Integer, Integer> numbersAndIndexes = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < n; i++) {
+            int newNumber = Integer.parseInt(numbers[i]);
+
+            if (numbersAndIndexes.containsKey(newNumber)) {
+                if (i - numbersAndIndexes.get(newNumber) <= k) {
+                    result = true;
+                    break;
+                }
+            }
+            numbersAndIndexes.put(newNumber, i);
+        }
+
+        if (result) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,10 @@
 package org.example;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import org.example.Homework3.HW3Task4;
 
 public class Main {
     public static void main(String[] args) {
-
+        HW3Task4 task4 = new HW3Task4();
+        task4.start();
     }
 }
